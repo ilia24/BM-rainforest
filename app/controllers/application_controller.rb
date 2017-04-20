92c07e03aead24f8a8272537986e73b4ce849fc2
ilia24 <iliademertchian24@gmail.com>
2017-04-20 11:11:07 -0400

    Add logic to display current user sign in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  # def current_user
+  #   session[:user_id] && User.find(session[:user_id])
+  # end
+
+  helper_method :current_user
 end
