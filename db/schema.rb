3b435ee705828a49aa4befd97b7c5cb49f82cd17
ilia24 <iliademertchian24@gmail.com>
2017-04-20 12:00:10 -0400

    Add name column to users, and display names througbh site as opposed to emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420151512) do
+ActiveRecord::Schema.define(version: 20170420155658) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170420151512) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
