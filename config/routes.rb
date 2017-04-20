142501f758777888e290aa9b89f83df20664fd8a
ilia24 <iliademertchian24@gmail.com>
2017-04-20 10:37:28 -0400

    Create session/login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
 
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
