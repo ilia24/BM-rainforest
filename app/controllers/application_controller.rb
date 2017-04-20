fbe60b97abdcbc8bd40ab73d231192c380f5f8aa
ilia24 <iliademertchian24@gmail.com>
2017-04-20 11:55:36 -0400

    Add authorization to ensure users are logged in before posting reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  def ensure_logged_in
+      unless current_user
+        flash[:alert] = "Please log in"
+        redirect_to new_session_url
+      end
+    end
 private
 
   def current_user
